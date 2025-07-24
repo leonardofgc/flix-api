6cdf55c5abe81610956c4927d2c14d50ef968ea1
Leonardo Guimarães <leonardofgc@gmail.com>
2025-07-24 17:26:00 -0300

    feat: Permissão de acesso e personalização do JWT

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
     path('autenthication/token/', TokenObtainPairView.as_view(), name="token_obtain_pais"),
     path('authentication/token/refresh/', TokenRefreshView.as_view(), name="token_refesh"),
+    path('authentication/token/verify/', TokenVerifyView.as_view(), name="token_verify"),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,14 +1,14 @@
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework import generics
 from movies.models import Movie
 from movies.serializers import MovieModelSerializer
 
 class MovieCreateListView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, IsAdminUser)
     queryset = Movie.objects.all()
     serializer_class = MovieModelSerializer
 
 class MovieRetriveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, IsAdminUser)
     queryset = Movie.objects.all()
     serializer_class = MovieModelSerializer
